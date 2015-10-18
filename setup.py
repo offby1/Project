65ee4b30c2d2e5d0201aa20fdbc7ad56d90fb81e
dgoodburn <dgoodburn@me.com>
2015-10-17 18:57:15 -0700

    1

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+#with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#    long_description = f.read()
 
 setup(
     name='Joynt',
@@ -19,7 +19,7 @@ setup(
     version='1.0.0',
 
     description='A Personal Finance Dashboard Project',
-    long_description=long_description,
+    #long_description=long_description,
 
     # The project's main homepage.
     url='https://github.com/dgoodburn/Project',
